5d997fd6dcbed79e4ee41dd8a1db1bb8e725644d
Parth Dhemeliya <parthd@mindfiresolutions.com>
2025-09-16 13:55:50 +0530

    fix(core): resolve delete key issue where text reappeared (#119)

--- packages/core/src/TextIgniter.ts
+++ packages/core/src/TextIgniter.ts
@@ -97,7 +97,7 @@ class TextIgniter {
     document.addEventListener('mouseup', () => {
       this.syncCurrentAttributesWithCursor();
       const dataId = this.document.getAllSelectedDataIds();
-      console.log(dataId, "dataId lntgerr")
+      console.log(dataId, 'dataId lntgerr');
     });
     document.getElementById('fontColor')?.addEventListener('click', e => {
       const fontColorPicker = document.getElementById(
@@ -245,7 +245,6 @@ class TextIgniter {
       this.document.dataIds[0] = jsonOutput[0].dataId;
       this.document.selectedBlockId = 'data-id-1734604240404';
       this.document.emit('documentChanged', this);
-      // eslint-disable-next-line no-unused-vars
       const [start, end] = this.getSelectionRange();
       this.document.blocks.forEach((block: any) => {
         if (this.document.dataIds.includes(block.dataId)) {
@@ -1018,7 +1017,7 @@ class TextIgniter {
         return;
       }
       const selection = window.getSelection();
-      console.log(selection,"selection lntgerr")
+      console.log(selection, 'selection lntgerr');
       if (this.document.dataIds.length >= 1 && this.document.selectAll) {
         this.document.deleteBlocks();
         this.setCursorPosition(start + 1);
@@ -1034,13 +1033,13 @@ class TextIgniter {
         const index = this.document.blocks.findIndex(
           (block: any) => block.dataId === this.document.selectedBlockId
         );
-        console.log(index,'index lntgerr')
+        console.log(index, 'index lntgerr');
         const chkBlock = document.querySelector(
           `[data-id="${this.document.selectedBlockId}"]`
         ) as HTMLElement;
         if (chkBlock === null) {
           let listStart = 0;
-          console.log(listStart," listStart lntgerr")
+          console.log(listStart, ' listStart lntgerr');
           const _blocks = this.document.blocks.map(
             (block: any, index: number) => {
               if (block?.listType !== undefined || block?.listType !== null) {
@@ -1055,7 +1054,7 @@ class TextIgniter {
               return block;
             }
           );
-          console.log(_blocks,"blocks lntgerr")
+          console.log(_blocks, 'blocks lntgerr');
           this.document.emit('documentChanged', this);
         }
       } else if (end > start) {
@@ -1130,7 +1129,7 @@ class TextIgniter {
     let fText = '';
 
     let count = 0;
-    console.log(count,"count lntgerr")
+    console.log(count, 'count lntgerr');
     const _block = this.document.blocks.filter((block: any) => {
       if (block.dataId === dataId) {
         return block;
